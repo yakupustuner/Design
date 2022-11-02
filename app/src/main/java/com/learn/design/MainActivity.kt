c7482d4a1e443302b13a26580359650c07e608d7
yakup <yakupustuner1@gmail.com>
2022-11-02 18:52:34 +0300

    Initial commit

---
+++ app/src/main/java/com/learn/design/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.learn.design
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+
+}
